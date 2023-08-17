4bf83b4b7901da56be6d67a81b724a1f534c21d8
Prince Boamah <regioths@gmail.com>
2023-08-17 01:19:50 +0000

    modified

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,7 +1,15 @@
--- Creates the user user_0d_1 with all privileges.
-CREATE USER
-    IF NOT EXISTS 'user_0d_1'@'localhost'
-    IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES
-   ON *.*
-   TO 'user_0d_1'@'localhost';
+-- Connect to the MySQL server
+-- Replace 'username' and 'password' with appropriate credentials
+mysql -u username -p
+
+-- Create user user_0d_1
+CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'password_here';
+
+-- Grant privileges to user_0d_1
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+
+-- Flush privileges to apply changes
+FLUSH PRIVILEGES;
+
+-- Exit the MySQL client
+exit;
