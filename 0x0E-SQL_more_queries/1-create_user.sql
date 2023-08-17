401852673c30f0924f8e007eb4e2d32227722ea9
Prince Boamah <regioths@gmail.com>
2023-08-17 01:23:15 +0000

    modified

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,15 +1,23 @@
--- Connect to the MySQL server
--- Replace 'username' and 'password' with appropriate credentials
-mysql -u username -p
+#!/bin/bash
 
--- Create user user_0d_1
-CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'password_here';
+# MySQL credentials
+MYSQL_USER="your_mysql_username"
+MYSQL_PASSWORD="your_mysql_password"
 
--- Grant privileges to user_0d_1
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+# Check if the user already exists
+EXISTING_USER=$(mysql -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" -e "SELECT COUNT(*) FROM mysql.user WHERE user = 'user_0d_1'" -N)
 
--- Flush privileges to apply changes
-FLUSH PRIVILEGES;
-
--- Exit the MySQL client
-exit;
+if [ "$EXISTING_USER" -eq 0 ]; then
+  # Create the user
+  mysql -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" -e "CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';"
+  
+  # Grant all privileges
+  mysql -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" -e "GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;"
+  
+  # Apply changes
+  mysql -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" -e "FLUSH PRIVILEGES;"
+  
+  echo "User 'user_0d_1' created with all privileges."
+else
+  echo "User 'user_0d_1' already exists. No changes made."
+fi
